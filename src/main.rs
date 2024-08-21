b2f8a0cfa34fb303edb05128a605c3877a41c413
Jon Heinritz <jon.heinritz@protonmail.com>
2024-08-21 15:45:30 +0200

    feat: add tracing-subscriber to binary

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
     let server = Arc::new(Server::default());
     server.run().await?;
 
