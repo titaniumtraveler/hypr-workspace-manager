ec1c687b35b9f1a431baa7d6ab67d1c2181cb209
Jon Heinritz <jon.heinritz@protonmail.com>
2024-09-02 00:56:48 +0200

    feat: start CLI from `main.rs` and setup logging
    At some point I'm going to have to fix the logging to make it
    "production ready", but at least for testing this is great!

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
-use hypr_workspace_manager::server::Server;
-use std::sync::Arc;
-use tracing::{error, info_span, Instrument};
+use clap::Parser;
+use hypr_workspace_manager::cli::Cli;
+
+#[allow(dead_code)]
+fn tracing_flat() {
+    use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_span_events(FmtSpan::FULL)
+        .init();
+}
+
+#[allow(dead_code)]
+fn tracing_tree() {
+    use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, Registry};
+    use tracing_tree::HierarchicalLayer;
+    Registry::default().with(HierarchicalLayer::new(4)).init();
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::init();
-    let server = Arc::new(Server::default());
-    async {
-        server
-            .run()
-            .await
-            .inspect_err(|err| error!(?err, "socket server failed to run"))
-    }
-    .instrument(info_span!("socket server"))
-    .await?;
+    tracing_tree();
+    Cli::parse().run().await?;
 
     Ok(())
 }
